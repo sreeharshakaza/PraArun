1e86f8afd898f74fe80e0a191b8076a28e14e794
sreeharsha <harsha@indavest.com>
2015-02-21 11:26:48 +0530

    Class2
    Razor and Html Helpers

---
+++ MVCClass/MVCClass/Controllers/RazorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCClass.Controllers
+{
+    public class RazorController : Controller
+    {
+        //
+        // GET: /Razor/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HtmlHelper()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCClass/MVCClass/CustomHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCClass
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString CustomImg()
+        {
+            string helperString = "<img src='../../Content/themes/base/images/ui-icons_cd0a0a_256x240.png' width='100' height='100'/>";
+
+            return new MvcHtmlString(helperString);
+        }
+    }
+}
